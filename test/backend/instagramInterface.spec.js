01a3e3ead7f1eb8bc518634619114038969e7b18
Brian Boyko <brian.boyko@gmail.com>
2016-10-21 11:42:23 -0500

    heading home -- safety commit

--- test/backend/instagramInterface.spec.js
+++ test/backend/instagramInterface.spec.js
@@ -4,6 +4,7 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 import request from 'request';
 import moment from 'moment';
+import bigInt from 'big-integer';
 
 import {makeEndpoint, getFromIGByTag, getThisManyPhotos} from '../../src/backend/instagramInterface';
 
@@ -22,6 +23,25 @@ describe("./src/backend/instagramInterface", function(){
     it('gets a number of photos', function(done){
       this.timeout(20000)
       expect(getThisManyPhotos(60, "furball").then((bunch) => bunch.data)).to.eventually.have.length(60).notify(done);
+    })
+  })
+  describe('solving the puzzle', function(){
+    it('trying to solve the puzzle. ', function(done){
+      expect(getFromIGByTag("cats").then((igResp) => {
+        let maxId = igResp.pagination.next_max_id;
+        let minId = igResp.pagination.next_min_id;
+        let dateMax = igResp.data[0].created_time
+        let dateMin = igResp.data[igResp.data.length - 1].created_time;
+// what if we just arbitrarily add a number?
+        console.log("dateMax:", dateMax);
+        console.log("dateMin:", dateMin);
+        console.log("maxId(radix 64)", bigInt(maxId, 64).toString());
+        console.log("minId(radix 64)", bigInt(minId, 64).toString());
+        console.log("difference dateMax - dateMin", dateMax - dateMin);
+        console.log("difference maxID - minId", bigInt(maxId, 64).subtract(bigInt(minId, 64)))
+        return true;
+      })).to.eventually.equal(true).notify(done);
+
     })
   })
 });
