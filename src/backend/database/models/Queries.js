01db50d1ebd712cde0af835215eea2157686d7e1
Brian Boyko <brian.boyko@gmail.com>
2016-10-24 15:17:50 -0500

    Got rid of more tags_queries cruft

--- src/backend/database/models/Queries.js
+++ src/backend/database/models/Queries.js
@@ -6,17 +6,17 @@ export default(knex) => {
 
   const tags = Tags(knex);
 
-  const create = (tag_name, startDate, endDate) => tags
-    .getOrAdd(tag_name)
-    .then(({id}) => knex('queries')
-      .insert({
-        tag_id: id,
-        earliest_date: moment.unix(parseInt(startDate)).toISOString(),
-        latest_date: moment.unix(parseInt(endDate)).toISOString(),
-        completed: false,
-        time_requested: moment(new Date()).toISOString(),
-      })
-      .returning('id'));
+  const create = (tag_name, startDate, endDate) => tags.getOrAdd(tag_name)
+      .then(({id}) => knex('queries')
+        .insert({
+          tag_id: id,
+          earliest_date: moment.unix(parseInt(startDate)).toISOString(),
+          latest_date: moment.unix(parseInt(endDate)).toISOString(),
+          completed: false,
+          time_requested: moment(new Date()).toISOString(),
+        })
+        .returning('id'));
+
 
   const read = {
     byId: (id) => knex('queries').where({id}).select(),
