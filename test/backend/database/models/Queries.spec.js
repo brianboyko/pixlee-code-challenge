d9b1284be5255e1510b42028b3b50fe2b08dbe90
Brian Boyko <brian.boyko@gmail.com>
2016-10-24 13:24:15 -0500

    finished Queries spec

--- test/backend/database/models/Queries.spec.js
+++ test/backend/database/models/Queries.spec.js
@@ -8,19 +8,53 @@ const expect = chai.expect;
 
 import knex from '../setup';
 import Queries from '../../../../src/backend/database/models/Queries';
+import Tags from '../../../../src/backend/database/models/Tags';
 
 const queries = Queries(knex);
+const tags = Tags(knex);
 
-describe('./src/backend/database/models/Media.js', function(){
+describe('./src/backend/database/models/Queries.js', function(){
   describe('queries.create()', function(){
+    before(function(done) {
+      knex.raw('truncate table ' + 'queries' + ' cascade').then(() => {
+        console.log("clearing ig_users data")
+        done()
+      });
+    })
+    var test_id;
     it('creates a query record', function(done){
       expect(queries.create("StephenFry", 1477181705, 1477181800)
       .then((idResponse) => {
         console.log("idResponse", idResponse);
+        test_id = idResponse[0];
         return idResponse[0];
       })).to.eventually.be.a('Number').notify(done);
-
-
+    })
+    it('retrives a query record (part 1)', function(done) {
+      expect(queries.read.byId(test_id)
+        .then((qRecord) => qRecord[0].tag_id)
+        .then((qTagId) => tags.read.byId(qTagId))
+        .then((tTagRecord) => tTagRecord[0].tag_name))
+        .to.eventually.equal("StephenFry").notify(done);
+    })
+    it('retrives a query record (part 2)', function(done) {
+      expect(queries.read.byId(test_id)
+        .then((qRecord) => _.pick(qRecord[0], ['earliest_date', 'latest_date', 'completed'])))
+        .to.eventually.eql({
+          completed: false,
+          earliest_date: moment.unix(1477181705).toDate(),
+          latest_date: moment.unix(1477181800).toDate(),
+        }).notify(done);
+    })
+    it('can complete a query', function(done) {
+      expect(queries.complete(test_id)
+        .then(() => queries.read.byId(test_id))
+        .then((records) => _.pick(records[0], 'completed', 'earliest_date', 'latest_date')))
+        .to.eventually.eql({
+          completed: true,
+          earliest_date: moment.unix(1477181705).toDate(),
+          latest_date: moment.unix(1477181800).toDate(),
+        }).notify(done);
     })
   })
 })
