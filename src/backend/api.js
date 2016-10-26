2fc12a6de4caca9022c4de2f4e1a1e332784cf5d
Brian Boyko <brian.boyko@gmail.com>
2016-10-26 15:22:05 -0500

    may need to rework database

--- src/backend/api.js
+++ src/backend/api.js
@@ -1,28 +1,24 @@
 // src/backend/api.js
 
 import Interface from './instagram/interface';
-const {getFromIGByTag, getPhotosInDateRange} = Interface;
 
+const { getFromIGByTag, getPhotosInDateRange } = Interface;
 
 export default (server, app) => {
   app.get('/api/test', (req, res) => {
-    res.send("I'm in L.A. My highlights look okay.")
-  })
+    res.send("I'm in L.A. My highlights look okay.");
+  });
 
   app.get('/api/getLatest/:tagname', function (req, res) {
     getFromIGByTag(req.params.tagname)
       .then((data) => JSON.stringify(data))
-      .then((jsonData) => res.send(jsonData))
+      .then((jsonData) => res.send(jsonData));
   });
 
   app.post('/api/createcollection', function (req, res) {
-    // req.body contains parameters "tagName", "startDate", "endDate"
-    console.log("req.body", req.body); 
-    getPhotosInDateRange(req.body.tagName, {startDate: req.body.startDate, endDate: req.body.endDate})
+    getPhotosInDateRange(req.body.tagName, { startDate: req.body.startDate, endDate: req.body.endDate })
       .then((bundle) => {
-        res.send(JSON.stringify(bundle, null, 2))
-      })
+        res.send(bundle);
+      });
   });
-
-
-}
+};
