506bd98c717556e28f05f8e133bd5e01bf5fc854
Brian Boyko <brian.boyko@gmail.com>
2016-10-30 19:01:26 -0500

    test

--- src/frontend/containers/Hero.js
+++ src/frontend/containers/Hero.js
@@ -57,7 +57,7 @@ class Hero extends Component {
 
     request({
       method: 'POST',
-      url: document.location.origin + 'api/createcollection',
+      url: document.location.origin + '/api/createcollection',
       headers: {
         'cache-control': 'no-cache',
         'content-type': 'application/json'
